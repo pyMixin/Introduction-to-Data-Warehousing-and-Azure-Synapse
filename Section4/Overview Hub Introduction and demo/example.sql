3ac92af53ff925a91a1af87279d947b51474dc18
pymixin <39064771+pyMixin@users.noreply.github.com>
2020-07-26 17:51:46 -0700

    Create example.sql

---
+++ Section4/Overview Hub Introduction and demo/example.sql
@@ -0,0 +1,18 @@
+SELECT  *   FROM OPENROWSET(
+          BULK  'abfss://azureudemycoursefs@azureudemycourseadls.dfs.core.windows.net/datasets_33080_1320127_car data.csv', 
+            FORMAT  =   'CSV', 
+            FIELDTERMINATOR  = ',', 
+            ROWTERMINATOR  =   '\n'     
+) WITH (
+  [Car_Name] VARCHAR (100)  COLLATE Latin1_General_BIN2, 
+  [Year] smallint, 
+  [Selling_Price] VARCHAR (100)  COLLATE Latin1_General_BIN2, 
+  [Present_Price] VARCHAR (100)  COLLATE Latin1_General_BIN2, 
+  [Kms_Driven] bigint, 
+  [Fuel_Type] VARCHAR (100)  COLLATE Latin1_General_BIN2, 
+  [Seller_Type VARCHAR (100)  COLLATE Latin1_General_BIN2, 
+  [Transmission] VARCHAR (100)  COLLATE Latin1_General_BIN2, 
+  [Owner] VARCHAR (100)  COLLATE Latin1_General_BIN2
+)  AS [r] 
+WHERE  
+  Car_Name ='ritz' AND year=2014
